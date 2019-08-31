20330c1f015d7c9a299ba6c67f6ebd1211baa6ab
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-08-31 10:17:43 +0900

    Update complaint.js

--- models/complaint.js
+++ models/complaint.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 let complaintSchema = new mongoose.Schema({
   issueid: String,
+  localid: { type: Number, default: 0 },
   title: String,
   image: String,
   description: String,
   coordinate: Object,
+  liked: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
-  liked: { type: Number, default: 0 }
+  isopen: { type: Boolean, default: true },
+  ispublic: { type: Boolean, default: true }
 });
 
 module.exports = mongoose.model('complaint', complaintSchema);
