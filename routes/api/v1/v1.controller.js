3fa907dd3fbe3a101e97b93e2866656b9389256f
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-08-31 10:17:55 +0900

    지역 구분 추가

--- routes/api/v1/v1.controller.js
+++ routes/api/v1/v1.controller.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
 
-const mongoUser = require('modules/userGetter');
+const setting = require('modules/setting');
 
-const connection = mongoose.connect(`mongodb://${mongoUser.id}:${mongoUser.pw}@db.donote.co/problert?authSource=admin`, { useNewUrlParser: true }).then(() => {
+const connection = mongoose.connect(`mongodb://${setting.id}:${setting.pw}@db.donote.co/problert?authSource=admin`, { useNewUrlParser: true })
+  .catch(err => {
+    console.error(err);
+  });
 
-}).catch(err => {
-  console.error(err);
+const instance = axios.create({
+  baseURL: 'https://dapi.kakao.com/v2/local/geo/',
+  timeout: 10000,
+  headers: {'Authorization': `KakaoAK ${setting.kakaoid}`}
 });
 
 const Complaint = require('models/complaint');
@@ -23,33 +29,105 @@ const randomString = (length) => {
 };
 
 exports.newIssue = (req, res, next) => {
-  let issueid = randomString(64);
-  let complaint = new Complaint({
-    "issueid": issueid,
-    "title": req.body.title,
-    "image": req.body.image,
-    "description": req.body.description,
-    "coordinate": {
-      "type": "Point",
-      "coordinates": [parseFloat(req.body.lng), parseFloat(req.body.lat)]
+  let lat = parseFloat(req.body.lat);
+  let lng = parseFloat(req.body.lng);
+  instance.get(`/coord2regioncode.json`, {
+    params: {
+      x: lng,
+      y: lat
     }
-  });
-
-  complaint.save(err => {
-    if (err) {
-      console.error(err);
-      res.send({ "success": false});
-      return;
+  }).then(response => {
+    let localid;
+    switch (response.data.documents[0].region_1depth_name) {
+      case "서울특별시":
+        localid = 0;
+        break;
+      case "부산광역시":
+        localid = 1;
+        break;
+      case "대구광역시":
+        localid = 2;
+        break;
+      case "인천광역시":
+        localid = 3;
+        break;
+      case "광주광역시":
+        localid = 4;
+        break;
+      case "대전광역시":
+        localid = 5;
+        break;
+      case "울산광역시":
+        localid = 6;
+        break;
+      case "세종특별자치시":
+        localid = 7;
+        break;
+      case "경기도":
+        localid = 8;
+        break;
+      case "강원도":
+        localid = 9;
+        break;
+      case "충청북도":
+        localid = 10;
+        break;
+      case "충청남도":
+        localid = 11;
+        break;
+      case "전라북도":
+        localid = 12;
+        break;
+      case "전라남도":
+        localid = 13;
+        break;
+      case "경상북도":
+        localid = 14;
+        break;
+      case "경상남도":
+        localid = 15;
+        break;
+      case "제주특별자치도":
+        localid = 16;
+        break;
+      default:
+        res.sendStatus(400);
+        return;
     }
-    res.send({
-      "success": true,
-      "issueid": issueid
+    let issueid = randomString(64);
+    let complaint = new Complaint({
+      "issueid": issueid,
+      "localid": localid,
+      "title": req.body.title,
+      "image": req.body.image,
+      "description": req.body.description,
+      "coordinate": {
+        "type": "Point",
+        "coordinates": [lng, lat]
+      }
     });
+    complaint.save(err => {
+      if (err) {
+        console.error(err);
+        res.send({ "success": false});
+        return;
+      }
+      res.send({
+        "success": true,
+        "issueid": issueid
+      });
+    });
+  }).catch(err => {
+    console.error(err);
   });
 };
 
 exports.getIssue = (req, res, next) => {
-  Complaint.findOne({"issueid": req.params.issueid}, {
+  Complaint.findOne({
+    "issueid": req.params.issueid,
+    "ispublic": true,
+    "isopen": true
+  }, {
     _id: 0,
     __v: 0
   }, (err, complaint) => {
